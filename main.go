dde526497c90ad268bcfc17507cd58c46f868300
Elahe Dastan <elahe.dstn@gmail.com>
2021-03-04 23:59:31 +0330

    all methods

--- main.go
+++ main.go
@@ -1,14 +1,64 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"regexp"
+)
+
+const (
+	GET = "GET"
+	POST = "POST"
+	PATCH = "PATCH"
+	DELETE = "DELETE"
+	PUT = "PUT"
 )
 
 func main() {
-	resp, err := http.Get("http://gobyexample.com")
+	// The first argument is always the URL
+	if len(os.Args) == 1 {
+		fmt.Println("URL is not given")
+		return
+	}
+
+	URL := os.Args[1]
+	if !Validate(URL) {
+		fmt.Println("URL is not in valid format")
+	}
+
+	method := flag.String("M", "GET", "method")
+	flag.CommandLine.Parse(os.Args[2:])
+
+	switch *method {
+	case GET:
+		do(URL, GET)
+	case POST:
+		do(URL, POST)
+	case PATCH:
+		do(URL, PATCH)
+	case DELETE:
+		do(URL, DELETE)
+	case PUT:
+		do(URL, PUT)
+	default:
+		fmt.Println("Method is not recognized. Use GET, POST, PUT, PATCH and DELETE instead")
+	}
+
+}
+
+func do(url string, method string) {
+	client := &http.Client{}
+
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := client.Do(req)
 	if err != nil {
 		panic(err)
 	}
@@ -23,4 +73,10 @@ func main() {
 
 	sb := string(body)
 	log.Println(sb)
+}
+
+func Validate(url string) bool {
+	var validURL = regexp.MustCompile(`https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)`)
+
+	return validURL.MatchString(url)
 }
