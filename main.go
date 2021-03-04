fd30a09895350b1d1d8934f2d816746474ff79de
Elahe Dastan <elahe.dstn@gmail.com>
2021-03-04 17:37:08 +0330

    simple GET

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
 func main() {
-	
+	resp, err := http.Get("http://gobyexample.com")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Response status:", resp.Status)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	sb := string(body)
+	log.Println(sb)
 }
